2960954a0b744430eb2a49a066f69bc19d02a605
deschi0000 <79345657+deschi0000@users.noreply.github.com>
2023-06-20 11:34:13 +0800

    update internal documentation

--- server/config/db.js
+++ server/config/db.js
@@ -1,8 +1,11 @@
 module.exports = {
   
+// Midterm Test "Favouite Book List Webapp" - Dave von Deschwanden - 301303220 - June 20.2023 - books.js
+
   //local MongoDB deployment ->
   // "URI": "mongodb://localhost/books229"
 
   // Connecting to MongoDbAtlas
   "URI": "mongodb+srv://dvondesc:qcx5KCzVsnkRiguk@mongodbserver.mwb7taf.mongodb.net/books229?retryWrites=true&w=majority"
+  
 };
