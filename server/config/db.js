400a58b0e598baadb8ab2358e087fc2084f64ba5
deschi0000 <79345657+deschi0000@users.noreply.github.com>
2023-06-20 09:47:39 +0800

    update URI for mongoDBAtlas

--- server/config/db.js
+++ server/config/db.js
@@ -1,4 +1,8 @@
 module.exports = {
+  
   //local MongoDB deployment ->
-  "URI": "mongodb://localhost/books229"
+  // "URI": "mongodb://localhost/books229"
+
+  // Connecting to MongoDbAtlas
+  "URI": "mongodb+srv://dvondesc:qcx5KCzVsnkRiguk@mongodbserver.mwb7taf.mongodb.net/books229?retryWrites=true&w=majority"
 };
